8710db260a7003e888136a5f2173979ebc08e3f4
Anna Shrubok <annashrubok21@gmail.com>
2024-11-15 21:04:45 +0300

    feat(commangline): add parser for array of args

---
+++ src/main/java/backend/academy/project/commandline/CommandLineArgsParser.java
@@ -0,0 +1,15 @@
+package backend.academy.project.commandline;
+
+import com.beust.jcommander.JCommander;
+
+public class CommandLineArgsParser {
+
+    public static CommandLineArgs getArgs(String[] args){
+        CommandLineArgs jArgs = new CommandLineArgs();
+        JCommander helloCmd = JCommander.newBuilder()
+            .addObject(jArgs)
+            .build();
+        helloCmd.parse(args);
+        return jArgs;
+    }
+}
