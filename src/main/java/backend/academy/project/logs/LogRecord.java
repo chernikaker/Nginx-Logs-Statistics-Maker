5cf15ed3ae798a5fb44d00369a5ad44a6d73d1a2
Anna Shrubok <annashrubok21@gmail.com>
2024-11-15 16:34:24 +0300

    feat(log): add getter by field name

--- src/main/java/backend/academy/project/logs/LogRecord.java
+++ src/main/java/backend/academy/project/logs/LogRecord.java
@@ -1,5 +1,6 @@
 package backend.academy.project.logs;
 
+import backend.academy.project.commandline.FilterFieldType;
 import java.time.LocalDateTime;
 
 @SuppressWarnings("RecordComponentNumber")
@@ -8,11 +9,28 @@ public record LogRecord(
      String remoteUser,
      LocalDateTime timeLocal,
      RequestType requestType,
-     String requestedResource,
+     String requestResource,
      String httpVersion,
      int status,
      long bytesSent,
      String httpReferer,
      String httpUserAgent
-    )
-{}
+    ) {
+
+    public String getValueByFieldName(FilterFieldType type) {
+        return switch (type) {
+            case REMOTE_ADDRESS -> remoteAddress;
+            case REMOTE_USER -> remoteUser;
+            case TIME_LOCAL -> timeLocal.toString();
+            case REQUEST_TYPE -> requestType.toString();
+            case REQUEST_RESOURCE -> requestResource;
+            case HTTP_VERSION -> httpVersion;
+            case STATUS -> String.valueOf(status);
+            case BYTES_SENT -> String.valueOf(bytesSent);
+            case HTTP_REFERER -> httpReferer;
+            case HTTP_USERAGENT -> httpUserAgent;
+            case NONE -> null;
+        };
+    }
+}
+
